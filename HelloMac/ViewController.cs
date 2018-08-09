1b4696946be3d8f1d56340a36341f42891de8cbe
Temani Beck <temanibeck@gmail.com>
2018-08-08 21:20:18 -0500

    Initial commit

---
+++ HelloMac/ViewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace HelloMac
+{
+    public partial class ViewController : NSViewController
+    {
+        private int numberOfTimesClicked = 0;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Set the initial value for the label
+            ClickedLabel.StringValue = "Button has not been clicked yet.";
+        }
+
+        partial void ClickedButton(NSObject sender)
+        {
+            // Update counter and label
+            ClickedLabel.StringValue = string.Format("The button has been clicked {0} time{1}.", ++numberOfTimesClicked, (numberOfTimesClicked < 2) ? "" : "s");
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
